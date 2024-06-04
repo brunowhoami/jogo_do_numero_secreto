3a751104a672f3f42e149f2b8b33c3b0368091a6
brunowhoami <brnsmgspdmkr@gmail.com>
2024-06-04 14:49:01 -0300

    Jogo do Numero Secreto 1.0!

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagem(){
     exibirTextoNaTela("h1", "Jogo do número secreto");
-    exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 100");
 }
 
 exibirMensagem();
